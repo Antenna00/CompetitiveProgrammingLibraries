d49b813f7326b818350e6fa6e50fb3216e09e7e1
GoldenRecord <89250932+PlusUltra00@users.noreply.github.com>
2022-07-09 16:47:43 +0000

    Matrix calculation

---
+++ CppLibraries.cpp
@@ -0,0 +1,10 @@
+#include <array>
+#include <iomanip>
+#include <math.h>
+//回転行列
+int KaitenGyouretu( a, int b, int c){
+    cin >> a >> b >> d;
+    double rad = M_PI * d / 180;
+    cout << setprecision(24) << (long double)cos(rad) * a + (long double)-sin(rad) * b << ' '
+    << (long double)sin(rad) * a + (long double)cos(rad) * b << endl;
+}
--- Golden.cpp
+++
@@ -1,7 +0,0 @@
-#include <iomanip>
-#include <array>
-#include <cmath>
-
-template<class T> std::array<T, 2> rotateMatrix(T a, T b, T d) {
-    double rad = 
-}
