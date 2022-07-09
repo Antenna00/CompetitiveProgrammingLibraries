237421a99d839c82d43c2b60b8f881907cf3e590
RinKit6024 <butasan6024@gmail.com>
2022-07-10 00:33:05 +0900

    kai

--- CppLibraries.cpp
+++ CppLibraries.cpp
@@ -1,10 +1,9 @@
 //二人の子供
 include <iostream>
 
+
 //回転行列
-int KaitenGyouretu(){
-    double a, b, d;
-    cin >> a >> b >> d;
+int KaitenGyouretu(double a, double b, double d){
     double rad = M_PI * d / 180;
     cout << setprecision(24) << (long double)cos(rad) * a + (long double)-sin(rad) * b << ' '
     << (long double)sin(rad) * a + (long double)cos(rad) * b << endl;
