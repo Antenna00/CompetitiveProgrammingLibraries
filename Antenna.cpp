5eba46738dc450ba2bc5e3c5c8fcb666223e3099
RinKit6024 <butasan6024@gmail.com>
2022-07-14 18:11:23 +0900

    antenna

--- Antenna.cpp
+++ Antenna.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+
+using namespace std;
 
 template<class T> long long findFactorial(T nElem){
 	long long temp = 1;
@@ -8,5 +13,21 @@ template<class T> long long findFactorial(T nElem){
  	return temp;
 }
 
+template<class T> long long convertStrToInt(T str){
+	int temp = atoi(str.c_str());
+	return temp;
+}
+
+template<class T> string convertIntToString(T value){
+	stringstream ss;
+	ss << value;
+	string temp = ss.str();
+	return temp;
+}
+
+template<class T> string convertIntToString2(T value){
+	string temp = to_string(value);
+	return temp;
+}
 
 
